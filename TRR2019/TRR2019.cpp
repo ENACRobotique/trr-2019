6514217ff4b0eb154d5fb756d0bbac1cad3b445f
EnacRobotique <robotique.enac@gmail.com>
2019-09-03 18:47:43 +0200

    Lecture

--- TRR2019/TRR2019.cpp
+++ TRR2019/TRR2019.cpp
@@ -2,13 +2,11 @@
 
 #include "Metro.h"
 #include "params.h"
-#include "Servo.h"
 #include "lidar/LidarXV11.h"
 //#include "lidar/LidarManager.h"
 
 
 
-Servo servo = Servo();
 LidarXV11 lidar = LidarXV11();
 bool isLidarSet;
 Metro lidarAsservTime(500);
@@ -16,59 +14,21 @@ unsigned int lidarPWM;
 unsigned int startTime;
 
 
-void command() { // Updates the servo command
-	int NB_LIDAR_ANGLES = 5; // <= 8
-	int TURN_DISTANCE = 1500;
-	int distanceD;
-	int distanceG;
-
-	int servoAngleCommand = 90;
-	int i = 2;
-	while (i <= NB_LIDAR_ANGLES && servoAngleCommand == 90) {
-		distanceG = lidar.get_distance(i * 10);
-		if (!lidar.is_valid(i * 10)) {
-			distanceG = 3000;
-		}
-		distanceD = lidar.get_distance(360 - i * 10);
-		if (!lidar.is_valid(360 - i * 10)) {
-			distanceD = 3000;
-		}
-
-		Serial.print(i);
-		Serial.print("		");
-		Serial.print(distanceG);
-		Serial.print("		");
-		Serial.print(lidar.is_valid(i * 10));
-		Serial.print("		");
-		Serial.print(distanceD);
-		Serial.print("		");
-		Serial.println(lidar.is_valid(360 - i * 10));
-
-
-		if (distanceG < TURN_DISTANCE || distanceD < TURN_DISTANCE) {
-			if (distanceG - distanceD > 100) {
-				//servoAngleCommand = 120 - (i - 1) * 4;
-				Serial.println("coucou G");
-				servoAngleCommand = 60 + (i - 1) * 4;
-			}
-
-			else if (distanceG - distanceD < -100) {
-				//servoAngleCommand = 60 + (i - 1) * 4;
-				Serial.println("coucou D");
-				servoAngleCommand = 120 - (i - 1) * 4;
-			}
-		}
-		i++;
+void dataGather(){
+	int distances[181];
+	for(int angle = 270; angle < 360; angle++){
+		distances[angle - 270] = lidar.get_distance(angle);
+		Serial.print(angle);
+		Serial.print("   ");
+		Serial.println(distances[angle - 270]);
 	}
 
-	/*Serial.print("Commande :    ");
-	Serial.println(servoAngleCommand);*/
-	servoAngleCommand = min(max(60, servoAngleCommand), 120);
-
-
-	//Serial.println(servoAngleCommand);
-
-	servo.write(servoAngleCommand);
+	for(int angle = 0; angle <= 90; angle++){
+		distances[angle + 90] = lidar.get_distance(angle);
+		Serial.print(angle);
+		Serial.print("   ");
+		Serial.println(distances[angle + 90]);
+	}
 
 }
 
@@ -97,11 +57,8 @@ void setup()
 	lidarPWM = 60;
 	Serial.begin(115200);
 	Serial2.begin(115200);
-	servo.attach(SERVO2);
 	pinMode(MOT1_DIR, OUTPUT);
-	digitalWrite(MOT1_DIR, HIGH);
-	servo.write(90);
-	//while(!Serial);
+	while(!Serial);
 	startTime = millis();
 }
 
@@ -110,9 +67,7 @@ void setup()
 // The loop function is called in an endless loop
 void loop()
 {
-	if (millis() - startTime > 4000){
-		analogWrite(MOT1_PWM, 50);
-	}
+
 	uint8_t b = 0;
 	if(Serial2.available()){
 		b = Serial2.read();
@@ -128,7 +83,8 @@ void loop()
 	Serial.println(lidar.is_valid(10));
 	Serial.print("    ");
 */
-	command();
+	//command();
+	dataGather();
 	asservLidar();
 
 }
